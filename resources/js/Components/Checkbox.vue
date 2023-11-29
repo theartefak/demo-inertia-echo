d0262e34e67b9dad58a19b3f59cbbb2af649343b
Artefak <the4rtefak@gmail.com>
2023-11-30 02:38:36 +0800

    [Fix] Issue on light mode

--- resources/js/Components/Checkbox.vue
+++ resources/js/Components/Checkbox.vue
@@ -37,7 +37,7 @@ const proxyChecked = computed({
                 type="checkbox"
                 :value="value"
                 v-model="proxyChecked"
-                class="rounded border-none bg-white text-primary shadow-sm ring-1 ring-gray-950/10 transition duration-75 checked:ring-0 focus:ring-2 focus:ring-primary focus:ring-offset-0 checked:focus:ring-primary-500/50 disabled:pointer-events-none disabled:bg-gray-50 disabled:text-gray-50 disabled:checked:bg-current disabled:checked:text-gray-400 dark:bg-white/5 dark:text-primary-500 dark:ring-white/20 dark:checked:bg-primary-500 dark:focus:ring-primary-500 dark:checked:focus:ring-primary-400/50 dark:disabled:bg-transparent dark:disabled:ring-white/10 dark:disabled:checked:bg-gray-600"
+                class="rounded border-none bg-white text-primary-600 shadow-sm ring-1 ring-gray-950/10 transition duration-75 checked:ring-0 focus:ring-2 focus:ring-primary focus:ring-offset-0 checked:focus:ring-primary-500/50 disabled:pointer-events-none disabled:bg-gray-50 disabled:text-gray-50 disabled:checked:bg-current disabled:checked:text-gray-400 dark:bg-white/5 dark:text-primary-500 dark:ring-white/20 dark:checked:bg-primary-500 dark:focus:ring-primary-500 dark:checked:focus:ring-primary-400/50 dark:disabled:bg-transparent dark:disabled:ring-white/10 dark:disabled:checked:bg-gray-600"
                 v-bind="$attrs" />
 
             <span
